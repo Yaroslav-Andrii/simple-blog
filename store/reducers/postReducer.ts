8320e464200141adcc25711cad0871fae0c10b6d
Yaroslav-Andrii <instructormedusa@gmail.com>
2020-09-20 14:25:34 +0300

    feat(store/postReducer): create actions handlers

--- store/reducers/postReducer.ts
+++ store/reducers/postReducer.ts
@@ -1,8 +1,26 @@
 import IAction from '../../interfaces/IAction';
 import IPost from '../../interfaces/IPost';
+import { PostTypes } from '../types';
 
 const handlers = {
   DEFAULT: (state: IPost[]) => state,
+  [PostTypes.INIT_POSTS]: (state: IPost[], action: IAction) => {
+    return action.payload
+  },
+  [PostTypes.DELETE_POST]: (state: IPost[], action: IAction) => {
+    return state.filter((post) => post.id !== action.payload);
+  },
+  [PostTypes.CREATE_POST]: (state: IPost[], action: IAction) => {
+    return [...state, action.payload]
+  },
+  [PostTypes.UPDATE_POST]: (state: IPost[], action: IAction) => {
+    return state.map((post) => {
+      if (post.id === action.payload.id) {
+        return action.payload;
+      }
+      return post;
+    });
+  },
 }
 
 const initialState: IPost[] = [];
